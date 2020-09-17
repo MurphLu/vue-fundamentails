8e42d11743f5617cc074874d18bdb960677288e8
Murph.Lu <Murph.Lu@homecreditcfc.cn>
2020-09-17 15:56:11 +0800

    project init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,79 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <nav>
+        <ul>
+          <li class="nav-item">
+            <img class="logo" src="./assets/build-a-bot-logo.png" alt="" />
+            build-a-Bot
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <!-- <HomePage/> -->
+      <RobotBuilder />
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+// import HomePage from './home/HomePage.vue';
+import RobotBuilder from './builder/RobotBuilder.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    // HomePage,
+    RobotBuilder,
   },
 };
 </script>
 
 <style>
+body {
+  background: linear-gradient(to bottom, #64363C, #F4A7B9);
+  background-attachment: fixed;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color:#64363C;
   margin-top: 60px;
 }
+
+main{
+  margin: 0 auto;
+  padding: 30px;
+  background-color: white;
+  width: 1024px;
+  min-height: 300px;
+}
+
+header {
+  background-color: #64363C;
+  width: 1084px;
+  margin: 0 auto;
+}
+ul {
+  padding: 3px;
+  display: flex;
+}
+nav {
+  color: #F4A7B9;
+}
+.nav-item {
+  display: inline-block;
+  padding: 5px 10px;
+  font-size: 22px;
+  border-right: 1px solid #F4A7B9;
+}
+.logo {
+  vertical-align: middle;
+  height: 30px;
+}
+
 </style>
