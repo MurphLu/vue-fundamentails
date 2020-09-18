d2aacb0a344a29d6f8f341f82fe6955d0a7c636e
Murph.Lu <Murph.Lu@homecreditcfc.cn>
2020-09-18 17:39:05 +0800

    add collapse

--- src/builder/RobotBuilder.vue
+++ src/builder/RobotBuilder.vue
@@ -1,19 +1,21 @@
 <template>
   <div class="content">
      <div class="preview">
-      <div class="preview-content">
-        <div class="top-row">
-          <img :src="selectedRobot.head.src"/>
+       <CollapsibleSection>
+        <div class="preview-content">
+          <div class="top-row">
+            <img :src="selectedRobot.head.src"/>
+          </div>
+          <div class="middle-row">
+            <img :src="selectedRobot.leftArm.src" class="rotate-left"/>
+            <img :src="selectedRobot.torso.src"/>
+            <img :src="selectedRobot.rightArm.src" class="rotate-right"/>
+          </div>
+          <div class="bottom-row">
+            <img :src="selectedRobot.base.src"/>
+          </div>
         </div>
-        <div class="middle-row">
-          <img :src="selectedRobot.leftArm.src" class="rotate-left"/>
-          <img :src="selectedRobot.torso.src"/>
-          <img :src="selectedRobot.rightArm.src" class="rotate-right"/>
-        </div>
-        <div class="bottom-row">
-          <img :src="selectedRobot.base.src"/>
-        </div>
-      </div>
+      </CollapsibleSection>
       <button class="add-to-cart" @click="addToCart()">Add To Cart</button>
     </div>
     <div class="top-row">
@@ -83,6 +85,7 @@
 import availableParts from '../data/parts';
 import createdHookMixin from './created_hook_mixin';
 import PartsSelector from './PartsSelector.vue';
+import CollapsibleSection from '../shared/CollapsibleSection.vue';
 
 export default {
   name: 'RobotBuilder',
@@ -91,6 +94,7 @@ export default {
   // },
   components: {
     PartsSelector,
+    CollapsibleSection,
   },
   data() {
     return {
