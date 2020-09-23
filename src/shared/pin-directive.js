c81ccc10429dad439b88d288b579446f76bcdd64
Murph.Lu <Murph.Lu@homecreditcfc.cn>
2020-09-23 17:03:49 +0800

    pass data to directive

--- src/shared/pin-directive.js
+++ src/shared/pin-directive.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-param-reassign */
 export default {
-  bind: (element) => {
+  bind: (element, binding) => {
+    // if (binding.arg !== 'position') return;
+    Object.keys(binding.value).forEach((position) => {
+      element.style[position] = binding.value[position];
+    });
     element.style.position = 'absolute';
-    element.style.bottom = '5px';
-    element.style.right = '5px';
   },
 };
